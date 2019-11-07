28af5ded601f15e000d32954731343e3d5d8ee0c
Rodrigo Ordóñez <rodrigo.op1791@gmail.com>
2019-11-07 14:01:40 -0600

    Modelos
    Creación de modelos Comentario y Conversación.

---
+++ src/dataaccess/model/Conversacion.js
@@ -0,0 +1,15 @@
+const mongoose = require("../MongoConnect");
+const Mensaje = require("./Mensaje");
+const Schema = mongoose.Schema;
+
+var ConversacionSchema = new Schema({
+    usuarios:{
+        required: true,
+        type: Array,   
+    },
+
+    mensajes:{
+        required: true,
+        type: Array,
+    },
+});
