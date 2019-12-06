2766aaf31b9a557fd33d120fb3b2ed7afafee031
Rodrigo Ordóñez <rodrigo.op1791@gmail.com>
2019-12-06 16:03:03 -0600

    Cambios mínimos
    Corrección

--- src/routes/api.imagen.route.js
+++ src/routes/api.imagen.route.js
@@ -167,8 +167,8 @@ router.post('/img/prueba', upload.single('image'), (req, res, next)=>{
     }
 
    var username = req.body.username;
-   let fecha = new Date();
-   let path ='./imgs/' + username + '_' + new Date().getTime().toString() + '.jpg'
+   var fecha = new Date();
+   var path ='./imgs/' + username + '_' + new Date().getTime().toString() + '.jpg'
 
     /**
      * Creación del nuevo Objeto Imagen
