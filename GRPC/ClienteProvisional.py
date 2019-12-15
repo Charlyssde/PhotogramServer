2522748e60932bdf5fe13015eed89cd4500d2ead
Carlos Carrillo <texcharly9@gmail.com>
2019-12-15 02:29:55 -0600

    Cuestiones x

--- GRPC/ClienteProvisional.py
+++ GRPC/ClienteProvisional.py
@@ -4,19 +4,20 @@ import Chat_pb2 as structure
 import threading
 import datetime
 host = 'localhost'
-port = 50051
+port = 1337
 channel = grpc.insecure_channel(host + ':' + str(port))
 stub = str_grpc.ChatStub(channel)
-sender =  "Uno mas"
+sender =  'Aline'
 def main():
     ##stub = str_grpc.ChatStub(channel)
-    receiver = "Charly"
-    msj = structure.Mensaje(sender = sender, content = "Message 4 test", receiver = receiver)
-    response = stub.enviarMensaje(msj)
-    print(response)
+    receiver = "Aline"
+    #msj = structure.Mensaje(sender = sender, content = "Message test", receiver = receiver)
+    #response = stub.enviarMensaje(msj)
+    #print(response)
     threading.Thread(target=getMessages(), daemon=True, args={}).start()
 
 def getMessages():
+    print("RECIBIENDO")
     for x in stub.recibirMensajes( structure.Usuario(username = sender)):
         print(x)
 
--- GRPC/Server.py
+++ GRPC/Server.py
@@ -29,7 +29,8 @@ class ChatServer(str_grpc.ChatServicer):
                     id = self.messages[cont][0], sender = self.messages[cont][1], receiver = self.messages[cont][2], content = self.messages[cont][3])
                 if request.username == message.receiver:
                     yield message
-                    cont = cont + 1
+                    print(cont)
+                cont = cont + 1
     
     def enviarMensaje(self, request, context):
         cur = cnx.cursor()
@@ -54,19 +55,21 @@ class ChatServer(str_grpc.ChatServicer):
 def main():
     port = '1337'
 
-    with open('serverchat.key', 'rb') as f:
-        private_key = f.read()
-    with open('serverchat.crt', 'rb') as f:
-        certificate_chain = f.read()
+    #with open('serverchat.key', 'rb') as f:
+    #    private_key = f.read()
+    #with open('serverchat.csr', 'rb') as f:
+    #    certificate_chain = f.read()
 
-    server_credentials = grpc.ssl_server_credentials(
-      ((private_key, certificate_chain),))
+    #server_credentials = grpc.ssl_server_credentials(
+    #  ((private_key, certificate_chain,),))
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=100))
     str_grpc.add_ChatServicer_to_server(ChatServer(), server)
 
     print('Servidor iniciado')
-    server.add_secure_port('[::]:'+port, server_credentials)
+    server.add_insecure_port('localhost:' + str(port))
+    #server.add_secure_port('[::]:'+port, server_credentials)
+    #server.add_insecure_port('[::]:50051')
     server.start()
     
     try:
