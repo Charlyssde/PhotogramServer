437331197475e0ddc10cc3bc29c3122c6392d1c9
Carlos Carrillo <texcharly9@gmail.com>
2019-12-16 17:32:53 -0600

    Cancal seguro server

--- GRPC/Server.py
+++ GRPC/Server.py
@@ -55,20 +55,20 @@ class ChatServer(str_grpc.ChatServicer):
 def main():
     port = '1337'
 
-    #with open('serverchat.key', 'rb') as f:
-    #    private_key = f.read()
-    #with open('serverchat.csr', 'rb') as f:
-    #    certificate_chain = f.read()
+    with open('serverchat.key', 'rb') as f:
+        private_key = f.read()
+    with open('serverchat.crt', 'rb') as f:
+        certificate_chain = f.read()
 
-    #server_credentials = grpc.ssl_server_credentials(
-    #  ((private_key, certificate_chain,),))
+    server_credentials = grpc.ssl_server_credentials(
+      ((private_key, certificate_chain,),))
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=100))
     str_grpc.add_ChatServicer_to_server(ChatServer(), server)
 
     print('Servidor iniciado')
-    server.add_insecure_port('localhost:' + str(port))
-    #server.add_secure_port('[::]:'+port, server_credentials)
+    #server.add_insecure_port('localhost:' + str(port))
+    server.add_secure_port('[::]:'+port, server_credentials)
     #server.add_insecure_port('[::]:50051')
     server.start()
     
